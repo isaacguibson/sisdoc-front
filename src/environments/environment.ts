957c095c9e77649cb906ceb935d61916df78984d
isaacguibson <isaac.guibson@gmail.com>
2019-10-16 05:30:52 -0300

    Isaac Guibson

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://ec2-18-222-74-55.us-east-2.compute.amazonaws.com:8082/sisdoc-0.0.1-SNAPSHOT/',
+  apiUrl: 'http://ec2-3-19-240-47.us-east-2.compute.amazonaws.com:8082/sisdoc-0.0.1-SNAPSHOT/',
   appName: 'Sistema de Gerenciamento de Documentos',
   appAlias: 'sisdoc',
   appUUID: 'ea393197-8ef3-4298-82b6-b03a46080518'
